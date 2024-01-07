362565476517688f301f25ae4e26a67014575ffb
ioanaistrate <133128272+ioanaistrate@users.noreply.github.com>
2024-01-07 22:28:30 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('MAILHOST',"smtp.gmail.com");
+define('USERNAME',"laindemana6@gmail.com");
+define('PASSWORD',"dmniuphqqdwcdomq");
+define('SEND_FROM',"istrateioana39@gmail.com");
+define('SEND_FROM_NAME',"La Indemana");
+define('REPLY_TO',"laindemana6@gmail.com");
+define('REPLY_TO_NAME',"Alexia");
+
+?>
--- script.js
+++ script.js
@@ -23,53 +23,6 @@ const search = () =>{
     }
 }
 
-/*const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
-    select.addEventListener('click', () =>{
-        select.classList.toggle('select-clicked');
-        select.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open');
-    });
-
-    options.forEach(option => {
-        option.addEventListener('click', () =>{
-            selected.innerText = option.innerText;
-
-            select.classList.remove('select-clicked');
-            select.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
-
-            options.forEach(option => {
-                option.classList.remove('active');
-            });
-            option.classList.add('active');
-        });
-    });
-});
- <form>
-        <div class="dropdown">
-            <div class="select">
-                <span class="select">Filtrare</span>
-                <div class="caret"></div>
-            </div>
-            <ul class="menu">
-                <li class="active">Toate produsele</li>
-                <li>Cercei</li>
-                <li>Altele</li>
-            </ul>
-        </div>
-
-    </form>
-
-*/
-
 let cartIcon = document.querySelector('#cart-icon')
 let cart = document.querySelector('.cart')
 let closeCart = document.querySelector('#close-cart')
